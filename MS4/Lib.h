a33c3fd97d2bb85bfbeb30e079a0152064ad0a02
Mehrnoosh Sarang <106999802+mehhrrnoosh@users.noreply.github.com>
2024-04-04 15:20:15 -0400

    Create Lib.h

---
+++ MS4/Lib.h
@@ -0,0 +1,12 @@
+#ifndef SDDS_LIB_H
+#define SDDS_LIB_H
+
+namespace sdds {
+    const int SDDS_MAX_LOAN_DAYS = 15;
+    const int SDDS_TITLE_WIDTH = 30;
+    const int SDDS_AUTHOR_WIDTH = 15;
+    const int SDDS_SHELF_ID_LEN = 4;
+    const int SDDS_LIBRARY_CAPACITY = 5000;
+}
+
+#endif //SDDS_LIB_H
