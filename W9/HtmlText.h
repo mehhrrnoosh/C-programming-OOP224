ec6f97a5d6738dd6d9c5176b78b5963283050406
Mehrnoosh Sarang <106999802+mehhrrnoosh@users.noreply.github.com>
2024-04-04 14:59:51 -0400

    Create HtmlText.h

---
+++ W9/HtmlText.h
@@ -0,0 +1,20 @@
+#ifndef SDDS_HTMLTEXT_H__
+#define SDDS_HTMLTEXT_H__
+
+#include "Text.h"
+
+namespace sdds {
+  class HtmlText : public Text {
+    char* m_title {nullptr};
+
+  public:
+    HtmlText(const char* filename=nullptr, const char* title = nullptr);
+    HtmlText(const HtmlText &src);
+    HtmlText &operator=(const HtmlText &src);
+    ~HtmlText();
+
+    void write(std::ostream &os) const;
+  };
+}
+
+#endif // SDDS_HTMLTEXT_H__
